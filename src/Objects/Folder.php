85c5e7ef5214d2ff733f71bb094a0752eab35b46
Robin De Schepper <robin.deschepper93@gmail.com>
2018-12-08 17:56:37 +0100

    check if item_collection exists before accessing it + autoRequest feature to request item_collection when it is missing.

--- src/Objects/Folder.php
+++ src/Objects/Folder.php
@@ -5,6 +5,8 @@ use PhpBox\Box;
 use PhpBox\Collections\ItemCollection;
 
 class Folder extends Item {
+  public static $autoRequest = false;
+  private $self_check_flag = false; // Used in checkItems as recursion flag.
   protected $item_collection;
   protected $folder_upload_email, $sync_state, $can_non_owners_invite;
   protected $is_collaboration_restricted_to_enterprise, $allowed_shared_link_access_levels;
@@ -17,19 +19,38 @@ class Folder extends Item {
     $this->tryCollectionFromData($data, ItemCollection::class, "item_collection");
   }
 
+  private function checkItems() {
+    if($this->item_collection === NULL) {
+      if(self::$autoRequest === false) {
+        throw new \Exception("No item_collection in Folder object. Try calling request to get full folder object or add 'item_collection' to the fields parameter. Alternatively set Folder::\$autoRequest to true to automatically request missing item_collections.");
+      } elseif($this->self_check_flag) {
+        throw new \Exception("Folder item_collection autoRequest failed: item_collection not found after requesting folder object.");
+      } else {
+        $this->request();
+        $this->self_check_flag = true;
+        $this->checkItems();
+        $this->self_check_flag = false;
+      }
+    }
+  }
+
   public function getFiles() {
+    $this->checkItems();
     return new ItemCollection($this->box, array_filter($this->item_collection->getArrayCopy(), function($x) {return $x->isFile();}));
   }
 
   public function getFolders() {
+    $this->checkItems();
     return new ItemCollection($this->box, array_filter($this->item_collection->getArrayCopy(), function($x) {return $x->isFolder();}));
   }
 
   public function getItems() {
+    $this->checkItems();
     return $this->item_collection;
   }
 
   public function findItems($name) {
+    $this->checkItems();
     $ret = [];
     foreach ($this->item_collection as $key => $value) {
       if(strpos($name, $value->name) !== FALSE) $ret[] = $value;
@@ -38,12 +59,14 @@ class Folder extends Item {
   }
 
   public function getFileByName($name) {
+    $this->checkItems();
     $arr = array_filter(function($x){return $x->name == $name;},$this->getFiles()->getArrayCopy());
     if(count($arr) === 1) return $arr[0];
     return false;
   }
 
   public function getFolderByName($name) {
+    $this->checkItems();
     $arr = array_filter(function($x){return $x->name == $name;},$this->getFolders()->getArrayCopy());
     if(count($arr) === 1) return $arr[0];
     return false;
