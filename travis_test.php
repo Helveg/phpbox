686dde10eb335bda2586884cec75cf3725768d51
Robin De Schepper <robin.deschepper93@gmail.com>
2018-12-08 11:02:33 +0100

    travis env test

--- travis_test.php
+++ travis_test.php
@@ -3,6 +3,14 @@
 include(__DIR__."/vendor/autoload.php");
 echo "Hello world! ".$_ENV['BOX_CLIENT_ID'];
 var_dump($_ENV);
+echo "a";
+var_dump($BOX_CLIENT_ID);
+echo "b";
+var_dump($_SERVER);
+echo "c";
+var_dump($GLOBALS);
+echo "d";
+var_dump($BOX_CLIENT_SECRET);
 exit(0);
 
 ?>
